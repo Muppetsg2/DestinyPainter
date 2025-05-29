be714f226c584c839a44cd58731fecd12858c879
Muppetsg2 <muppetsg2@gmail.com>
2025-05-29 22:13:38 +0200

    End Screen and Pause Menu Graphics

---
+++ Assets/Scripts/StarObject.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class StarObject : MonoBehaviour
+{
+    [SerializeField] private bool active = false;
+    [SerializeField] private Image image;
+    [SerializeField] private Shadow shadow;
+    [SerializeField] private TextMeshProUGUI textHolder;
+    [SerializeField][TextArea] private string text = "";
+
+    void Start()
+    {
+        SetStarActive(active);
+        SetStarText(text);
+    }
+
+    private void SetStarColor()
+    {
+        image.color = ColorsManager.Instance.GetStarColor(ColorCategory.Primary, active);
+        shadow.effectColor = ColorsManager.Instance.GetStarColor(ColorCategory.Secondary, active);
+    }
+
+    public void SetStarActive(bool value)
+    {
+        active = value;
+        SetStarColor();
+    }
+
+    public void SetStarText(string value)
+    {
+        text = value;
+        textHolder.text = text;
+    }
+}
